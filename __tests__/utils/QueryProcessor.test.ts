db2a6c4844cd360e29ba8ecd89d45431d0f34cc0
EliezerMCR <eliezermanuelcr@gmail.com>
2024-11-18 14:39:32 -0400

    add plus test type

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -31,14 +31,26 @@ describe('QueryProcessor', () => {
 		expect(response).toBe('Elizer');
 	});
 
-    test.each([
-        ["Which of the following numbers is the largest: 87, 55, 92?", "92"],
-        ["Which of the following numbers is the largest: 28, 44, 48?", "48"],
-        ["Which of the following numbers is the largest: 68, 19, 59?", "68"],
-        ["Which of the following numbers is the largest: 8, 61, 20?", "61"],
-        ["Which of the following numbers is the largest: 2, 7, 78?", "78"],
-      ])("should return the largest number for query '%s'", (query, expected) => {
-        const response: string = QueryProcessor(query);
-        expect(response).toBe(expected);
-      });
+	test.each([
+		['Which of the following numbers is the largest: 87, 55, 92?', '92'],
+		['Which of the following numbers is the largest: 28, 44, 48?', '48'],
+		['Which of the following numbers is the largest: 68, 19, 59?', '68'],
+		['Which of the following numbers is the largest: 8, 61, 20?', '61'],
+		['Which of the following numbers is the largest: 42, 79, 41?', '79'],
+		['Which of the following numbers is the largest: 2, 7, 78?', '78'],
+	])("should return the largest number for query '%s'", (query, expected) => {
+		const response: string = QueryProcessor(query);
+		expect(response).toBe(expected);
+	});
+
+	test.each([
+		['What is 85 plus 93?', '178'],
+		['What is 91 plus 5?', '96'],
+		['What is 17 plus 42?', '59'],
+		['What is 91 plus 32?', '123'],
+		['What is 85 plus 93?', '178'],
+	])("should return the sum for query '%s'", (query, expected) => {
+		const response: string = QueryProcessor(query);
+		expect(response).toBe(expected);
+	});
 });
